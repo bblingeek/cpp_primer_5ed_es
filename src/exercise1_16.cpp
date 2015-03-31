5fc2d254d64628902dd3c1905eb161b5f067a7cc
bblingeek <bblingeek@users.noreply.github.com>
2015-03-31 15:46:33 +0530

    Create exercise1_16.cpp

---
+++ src/exercise1_16.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name : exercise1_16.cpp
+// Author : Bhaskar Narayan Bhat
+// Version :
+// Copyright : Your copyright notice
+// Description : None
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int input = 0;
+
+	while(cin>>input)
+	{
+		sum += input;
+	}
+	cout<<"Sum is : "<<sum<<endl;
+
+	return 0;
+}
