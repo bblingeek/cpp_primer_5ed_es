5e1866c8d7c4c04b31d1ee5751d1cc1e17833bbd
bblingeek <bblingeek@users.noreply.github.com>
2015-03-31 15:34:56 +0530

    Create exercise1_4.cpp

---
+++ src/exercise1_4.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : exercise14.cpp
+// Author      : Bhaskar Narayan Bhat
+// Version     :
+// Copyright   : Your copyright notice
+// Description : None
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout<<"Enter two numbers:" <<endl;
+
+	int v1 = 0, v2 = 0;
+
+	cin>> v1 >> v2;
+
+	cout<<"The product of "<< v1 <<" and "<< v2
+    	<<" is " << v1 * v2 <<endl;
+
+    return 0;
+}
