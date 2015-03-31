5ed54810207da193ebcf69cd8d818e662df5fd76
bblingeek <bblingeek@users.noreply.github.com>
2015-03-31 15:30:19 +0530

    Create exercise1_2.cpp

---
+++ src/exercise1_2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : exercise12.cpp
+// Author      : Bhaskar Narayan Bhat
+// Version     :
+// Copyright   : Your copyright notice
+// Description : None
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	return -1;
+}
