c4f90f6b6517b98822194f986175973202539fcd
bblingeek <bblingeek@users.noreply.github.com>
2015-03-31 15:45:55 +0530

    Create exercise1_13_2.cpp

---
+++ src/exercise1_13_2.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name : exercise1_13_2.cpp
+// Author : Bhaskar Narayan Bhat
+// Version :
+// Copyright : Your copyright notice
+// Description : None
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int val = 0;
+
+	for(val = 10; val >= 0; val--)
+	{
+		cout<< val <<endl;
+	}
+
+    return 0;
+}
