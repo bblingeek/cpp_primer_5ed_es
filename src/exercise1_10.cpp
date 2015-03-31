e86f6d3f881ca31500a1f0e3b737043838df579c
bblingeek <bblingeek@users.noreply.github.com>
2015-03-31 15:37:51 +0530

    Create exercise1_10.cpp

---
+++ src/exercise1_10.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : exercise1_10.cpp
+// Author      : Bhaskar Narayan Bhat
+// Version     :
+// Copyright   : Your copyright notice
+// Description : None
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int val = 10;
+
+	while(val >= 0)
+	{
+		cout<< val <<endl;
+		val--;
+	}
+
+    return 0;
+}
