c426a5548c68f947168eeb4f58cb4b1544e6c355
bblingeek <bblingeek@users.noreply.github.com>
2015-03-31 15:47:25 +0530

    Create exercise1_20.cpp

---
+++ src/exercise1_20.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name : exercise1_20.cpp
+// Author : Bhaskar Narayan Bhat
+// Version :
+// Copyright : Your copyright notice
+// Description : None
+//============================================================================
+
+#include "Sales_item.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	Sales_item s;
+
+	while(cin>>s)
+	{
+		cout<<s<<endl;
+	}
+	return 0;
+}
