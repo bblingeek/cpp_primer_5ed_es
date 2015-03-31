85bfdbcbfde5157e033d47553c083391d2b46627
bblingeek <bblingeek@users.noreply.github.com>
2015-03-31 15:31:29 +0530

    Create exercise1_3.cpp

---
+++ src/exercise1_3.cpp
@@ -0,0 +1,15 @@
+/*
+ * exercise13.cpp
+ *
+ *  Created on: Mar 26, 2015
+ *      Author: Bhaskar Narayan Bhat
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout<<"Hello, World"<<endl;
+	return 0;
+}
