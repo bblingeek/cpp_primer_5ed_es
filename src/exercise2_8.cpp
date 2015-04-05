adc9aa05e6c9ba89e3e51300ef9a9b17287484f0
Bhaskar N Bhat <bblingeek@users.noreply.github.com>
2015-04-05 11:56:17 +0530

    Create exercise2_8.cpp

---
+++ src/exercise2_8.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name : exercise2_8.cpp
+// Author : Bhaskar Narayan Bhat
+// Version :
+// Copyright : Your copyright notice
+// Description : None
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout<<2<<"\115\012";
+	cout<<2<<"\t\115\012";
+	return 0;
+}
