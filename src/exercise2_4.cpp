9e86bff07173f20b1161e5ea83a6cc3cedb1d4de
Bhaskar N Bhat <bblingeek@users.noreply.github.com>
2015-04-05 11:19:49 +0530

    Create exercise2_4.cpp

---
+++ src/exercise2_4.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name : exercise2_4.cpp
+// Author : Bhaskar Narayan Bhat
+// Version :
+// Copyright : Your copyright notice
+// Description : None
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	unsigned u = 10, u2 = 42;
+	cout << u2 - u << endl;
+	cout << u - u2 << endl;
+	int i = 10, i2 = 42;
+	cout << i2 - i << endl;
+	cout << i - i2 << endl;
+	 
+	 
+	cout << i - u << endl;
+	cout << u - i << endl;
+	return 0;
+}
